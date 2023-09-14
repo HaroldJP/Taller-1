11346d1ea4e03715181c92a6b72e20a7b41057d4
HaroldJP <142249299+HaroldJP@users.noreply.github.com>
2023-09-13 19:07:36 -0500

    Add files via upload
    Este programa sirve para seleccionar 3 números reales cualesquiera y determinar cuál de los 3 es el número mayor.

---
+++ numero_mayor.py
@@ -0,0 +1,20 @@
+a : float
+b : float
+c : float
+a = float(input('Escriba el primer número real:'))
+b = float(input('Escriba el segundo número real:'))
+c = float(input('Escriba el tercer número real:'))
+if a <= b < c:
+    print('El número mayor es ' +str(c))
+elif a <= c < b:
+    print('El número mayor es ' +str(b))
+elif b <= a < c:
+    print('El número mayor es ' +str(c))
+elif b <= c < a:
+    print('El número mayor es ' +str(a))
+elif c <= a < b:
+    print('El número mayor es ' +str(b))
+elif c <= b < a:
+    print('El número mayor es ' +str(a))
+elif a == b == c:
+    print('El número mayor es ' +str(a))
