4e86bc4cefd9f732b049f660e677ca5c0c48a9c9
HaroldJP <142249299+HaroldJP@users.noreply.github.com>
2023-09-15 22:32:50 -0500

    Update numero_mayor.py

--- numero_mayor.py
+++ numero_mayor.py
@@ -4,17 +4,15 @@ c : float
 a = float(input('Escriba el primer número real:'))
 b = float(input('Escriba el segundo número real:'))
 c = float(input('Escriba el tercer número real:'))
-if a <= b < c:
+if a <= b <= c:
     print('El número mayor es ' +str(c))
-elif a <= c < b:
+elif a <= c <= b:
     print('El número mayor es ' +str(b))
-elif b <= a < c:
+elif b <= a <= c:
     print('El número mayor es ' +str(c))
-elif b <= c < a:
+elif b <= c <= a:
     print('El número mayor es ' +str(a))
-elif c <= a < b:
+elif c <= a <= b:
     print('El número mayor es ' +str(b))
-elif c <= b < a:
-    print('El número mayor es ' +str(a))
-elif a == b == c:
+elif c <= b <= a:
     print('El número mayor es ' +str(a))
